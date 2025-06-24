e88b0cae45c6ed14f52c3833a0b4830475af9e14
Elong-constance <elongconstance@gmail.com>
2025-06-24 05:11:59 -0700

    first commit

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,32 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import MovieList from './MovieList';
 
 
 const Home = () => {
-  const [Movies, Setmovies] = useState([
-    {title: "commando", body: "Action & Adventure Movies", actor: "Alnold", id:1},
-    {title: "Predator", body: "Action & Adventure & Herror Movie", actor: "Arnold,", id:2},
-    {title: "Tom Raider", body: "Action & Adventure Movie", actor: "Lara Croft", id:3},
+  const [Movies, Setmovies] = useState
 
-  ])
+   
+  useEffect(()=>{
+   fetch('http://localhost:8000/movies')
+    .then((res)=>{
+      return res.json()
+    })
+    .then((data)=>{
+      console.log(data)
+      SetMovies(data)
+    })
+  }, [])
 
-   const handleDelete = (id)=>{
-    const newMovie = movies.filter(movie=>movie.id !== id)
-    Setmovies(newMovie)
-  }
+    
   return (
     <div className='home'>
-        <MovieList Movies = {movies} title = "Movies Blog!!!"/> 
-       {/* trying to output Arnold Movies */}
-       <MovieList Movies={Movies.filter((movie)=>{
-        if(Movies.actor =="Arnord"){
-          return true
-        }
-      })} title = "Arnold's Movies"handleDelete={handleDelete}/>
+     {movies && <MovieList Movies = {movies} title = "Movie's Blog!!!"/> }
+      
       </div>
   )
 }
+
 export default Home
           
             
